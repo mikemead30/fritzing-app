1038ef7c0aa1bde9789b3b14b148e34f68abd817
mike <80270980+mikemead30@users.noreply.github.com>
2023-10-04 15:38:57 +0100

    Update build-windows.bat

--- tools/build-windows.bat
+++ tools/build-windows.bat
@@ -4,3 +4,4 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Too
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
 
 tools\release_fritzing.bat %1 64 2017
+
